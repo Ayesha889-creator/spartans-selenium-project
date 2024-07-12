f6a095bb371a803218e3ce0419bf4d4862d18562
Ayesha <starart.2024@gmail.com>
2024-07-11 20:27:23 -0400

    handle cross browsing

--- src/test/java/CrossBrowsing/CrossBrowsing.java
+++ src/test/java/CrossBrowsing/CrossBrowsing.java
@@ -4,53 +4,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 import java.lang.reflect.Type;
 
 public class CrossBrowsing {
-    public static void main(String[] args) {
-//        EdgeDriver driver= new EdgeDriver();
-//        FirefoxDriver driver1 = new FirefoxDriver();
-//        driver.manage().window().maximize();
-//        driver.get("https://support.google.com/chrome/");
-//        String title = driver.getTitle();
-//        System.out.println(title);
-//        driver.quit();
+    public static void main(String[] args) {}}
+////
+//        String browserType = "edge";
+//        WebDriver driver;
+//
+//        if (browserType.equals("chrome")) {
+//            driver = new ChromeDriver();
+//        } else if (browserType.equals(Edge))
+//            driver = new EdgeDriver();{
+//        } else if (browserType.equals("firefox")) {
+//                driver = FirefoxDriver();
+//        } else throw new RuntimeException("wrong browsertype");
+//    }
+//       driver.get("https://support.google.com/chrome/");
+//
+//       }}
 
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://tek-retail-ui.azurewebsites.net/");
-//        driver.manage().window().maximize();
-//        driver.getTitle();
-//        String title = driver.getTitle();
-//         driver.getCurrentUrl();
-//         String URL = driver.getCurrentUrl();
-//        System.out.println("URL");
-//         driver.quit();
-
-
-        String browserType = "edge";
-        WebDriver driver = null;
-
-
-        if (browserType.equals("edge")) {
-            driver = new ChromeDriver();
-        } else if (browserType.equals("edge")) {
-            driver = new EdgeDriver();
-        } else if (browserType.equals("firefox")) {
-
-        } else if (browserType.equals("Safari")) {
-            driver = new SafariDriver();
-        } else throw new RuntimeException("wrong browsertype");
-
-    
-
-       driver.get("https://support.google.com/chrome/");
-       driver.quit();
-       driver.manage().window().maximize();
-
-       }
-}
 
 
 
---
+++ src/test/java/tek/info/Activity2.java
@@ -0,0 +1,20 @@
+//package tek.info;
+//
+//import org.openqa.selenium.WebDriver;
+//
+//import java.util.Scanner;
+//
+//public class Activity2 {
+//    public static void main(String[] args) {
+//
+//       // activity is find a way to ask user where to run
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("choose browser, firefox and edge");
+//        String browserType = scanner.next();
+//        WebDriver;
+//        if browserType.equelsI
+//
+//
+//
+//    }
+//}
